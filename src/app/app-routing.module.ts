c24501a3125b3a851fd15d2d70ab4481f28af2b0
MaheninaHerii <maheninaheritoky@gmail.com>
2023-03-29 19:26:50 +0200

    Création page dashboard, statistique et inscription

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
+import { PageStatistiqueComponent } from './pages/page-statistique/page-statistique.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: PageLoginComponent
+  },
+  {
+    path:'inscrire',
+    component:PageInscriptionComponent
+  },
+  {
+    path:'',
+    component:PageDashboardComponent,
+      children:[
+        {
+          path:'statistique',
+          component:PageStatistiqueComponent
+        }
+   ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
